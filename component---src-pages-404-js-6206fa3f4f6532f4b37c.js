8b304d94d445c4fd1f007f81b835fce29549acb2
Brian Walshe <walshe.brian@gmail.com>
2020-04-13 20:47:49 +0100

    Overwriting with gatsby page

---
+++ component---src-pages-404-js-6206fa3f4f6532f4b37c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{w2l6:function(n,t,o){"use strict";o.r(t);o("q1tI");var u=o("qKvR");t.default=function(){return Object(u.b)("p",null,"404 - not found")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-6206fa3f4f6532f4b37c.js.map
